459d54af080322b8e3fded9d461b89f54b7da35d
Tim Chagnon <tachagnon@gmail.com>
2015-10-23 00:29:41 -0700

    Open SMF files and count vertices and faces lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: command <file.smf>");
+        return;
+    }
+
+    let path = &args[1];
+    let smf_file = match File::open(path) {
+        Ok(file) => file,
+        Err(why) => panic!("Could not open \"{}\": {}", path, why),
+    };
+    let input = BufReader::new(smf_file);
+
+    let lines   : Vec<_> = input.lines().map(|l| l.unwrap()).collect();
+    let vertices: Vec<_> = lines.iter().filter(|l| l.starts_with("v")).collect();
+    let faces   : Vec<_> = lines.iter().filter(|l| l.starts_with("f")).collect();
+    println!("vertices: {} faces: {}", vertices.len(), faces.len());
 }
