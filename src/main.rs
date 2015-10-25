a10cfd488ed49d0fe84ad6762aa5fdbaff5ad910
Tim Chagnon <tachagnon@gmail.com>
2015-10-25 16:01:19 -0700

    Parse smf files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
+
+mod mesh;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -11,14 +10,6 @@ fn main() {
     }
 
     let path = &args[1];
-    let smf_file = match File::open(path) {
-        Ok(file) => file,
-        Err(why) => panic!("Could not open \"{}\": {}", path, why),
-    };
-    let input = BufReader::new(smf_file);
-
-    let lines   : Vec<_> = input.lines().map(|l| l.unwrap()).collect();
-    let vertices: Vec<_> = lines.iter().filter(|l| l.starts_with("v")).collect();
-    let faces   : Vec<_> = lines.iter().filter(|l| l.starts_with("f")).collect();
-    println!("vertices: {} faces: {}", vertices.len(), faces.len());
+    let mesh = mesh::read_smf(path);
+    println!("vertices: {} faces: {}", mesh.vertices.len(), mesh.faces.len());
 }
---
+++ src/mesh.rs
@@ -0,0 +1,57 @@
+use std::str::FromStr;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+#[derive(Debug, Default, PartialEq)]
+pub struct Vec3f {
+    x: f32,
+    y: f32,
+    z: f32,
+}
+
+#[derive(Debug, Default, PartialEq)]
+pub struct Face {
+    a: i32,
+    b: i32,
+    c: i32,
+}
+
+#[derive(Debug, Default, PartialEq)]
+pub struct Mesh {
+    pub vertices: Vec<Vec3f>,
+    pub faces: Vec<Face>,
+}
+
+pub fn read_smf(path: &str) -> Mesh {
+    let smf_file = match File::open(path) {
+        Ok(file) => file,
+        Err(why) => panic!("Could not open \"{}\": {}", path, why),
+    };
+    let input = BufReader::new(smf_file);
+
+    let lines   : Vec<String> = input.lines().map(|l| l.unwrap()).collect();
+    let vertices: Vec<Vec3f> = lines.iter()
+        .filter(|l| l.starts_with("v "))
+        .map(read_vertex)
+        .collect();
+    let faces   : Vec<Face> = lines.iter()
+        .filter(|l| l.starts_with("f "))
+        .map(read_face)
+        .collect();
+    Mesh { vertices: vertices, faces: faces }
+}
+
+fn read_vertex(s: &String) -> Vec3f {
+    let v: Vec<f32> = s.split_whitespace()
+        .filter_map(|x| f32::from_str(x).ok())
+        .collect();
+    Vec3f { x: v[0], y: v[1], z: v[2] }
+}
+
+fn read_face(s: &String) -> Face {
+    let v: Vec<i32> = s.split_whitespace()
+        .filter_map(|x| i32::from_str(x).ok())
+        .collect();
+    Face { a: v[0], b: v[1], c: v[2] }
+}
